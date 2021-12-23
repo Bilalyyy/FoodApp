8e3fec3e0f99141feb98e48fa6c783941d221365
Bilal <yyybilal@gmail.com>
2021-12-23 08:21:14 +0100

    asset, base de donnee et un peu plus

---
+++ FoodApp/Model/Food.swift
@@ -0,0 +1,17 @@
+//
+//  Food.swift
+//  FoodApp
+//
+//  Created by bilal on 23/12/2021.
+//
+
+import Foundation
+
+struct Food {
+
+    var name: String
+    var desc: String
+    var country: String
+    var image: String
+
+}
---
+++ FoodApp/View/RoundedImage.swift
@@ -0,0 +1,29 @@
+//
+//  RoundedImage.swift
+//  FoodApp
+//
+//  Created by bilal on 23/12/2021.
+//
+
+import UIKit
+
+
+class RoundedImage: UIImageView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        self.layer.cornerRadius = self.frame.height / 2
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+}
